85132089da790bfce6877688f371b54d5cd3d5ec
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-03 13:06:00 +0530

    updated Contentn:

--- POS/src/App.tsx
+++ POS/src/App.tsx
@@ -1,10 +1,37 @@
-
+import {BrowserRouter as Router, Routes,Route,Link} from "react-router-dom";
 import './App.css'
+import Home from "./components/Home.tsx";
 
 function App() {
 
   return (
-    <h1>Hello Team!</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/customer'>Customer</Link>
+            </li>
+            <li>
+              <Link to='/product'>Product</Link>
+            </li>
+
+          </ul>
+        </nav>
+
+        <hr/>
+
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/customer' element={<Customer/>}></Route>
+          <Route path='/product' element={<Product/>}></Route>
+        </Routes>
+
+      </div>
+    </Router>
   )
 }
 
