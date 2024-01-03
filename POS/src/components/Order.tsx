5d059cc69bc6770262c07e72093afa79ed005018
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-03 17:45:27 +0530

    updated Contentn:

--- POS/src/components/Order.tsx
+++ POS/src/components/Order.tsx
@@ -4,61 +4,93 @@ function  Order(){
     const styleObj:React.CSSProperties={
         marginBottom:'20px'
     }
+    const bottomContext:React.CSSProperties={
+        width:'100%',
+        display:'flex',
+        alignItems:'center',
+        justyfyContent:'space-between'
+
+    }
 
     return (
         <>
             <br/>
             <div className='container'>
                 <div className="row">
-                    <div className="col-12 col-sm-6 col-md-4" style={styleObj}>
+                    <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
                         <div className="form-group">
-                            <label htmlFor="productName">Product Name</label>
-                            <input type="text" className='form-control' id='productName'/>
+                            <label htmlFor="customer">Select Customer</label>
+                            <select  id="customer" className='form-control'>
+                                <option value="#" disabled defaultValue='Use Option '>Use Option </option>
+                                <option value="#">Customer 01</option>
+                                <option value="#">Customer 02</option>
+                            </select>
                         </div>
                     </div>
-                    <div className="col-12 col-sm-6 col-md-4">
+                    <div className="col-12 col-sm-6 col-md-3">
                         <div className="form-group">
-                            <label htmlFor="price">Unit Price</label>
-                            <input type="text" className='form-control' id='price'/>
+                            <label htmlFor="name">Customer Name</label>
+                            <input type="text" disabled className='form-control' id='name'/>
                         </div>
                     </div>
-                    <div className="col-12 col-sm-6 col-md-4">
+                    <div className="col-12 col-sm-6 col-md-3">
                         <div className="form-group">
-                            <label htmlFor="qty">QTY on Hand</label>
-                            <input type="text" className='form-control' id='qty'/>
+                            <label htmlFor="address">Customer Address</label>
+                            <input type="text" disabled className='form-control' id='address'/>
                         </div>
                     </div>
-                    <div className="col-12 col-sm-6 col-md-4">
+                    <div className="col-12 col-sm-6 col-md-3">
                         <div className="form-group">
-                            <label htmlFor="image">Product Image </label>
-                            <input type="file" className='form-control' id='image'/>
-                        </div>
-                    </div>
-
-                    <div className="col-12 col-sm-6 col-md-4">
-                        <div className="form-group">
-                            <label htmlFor="descriptions">Descriptions</label>
-                            <input  className='form-control' id='descriptions'/>
+                            <label htmlFor="salary">Salary</label>
+                            <input type="number" disabled className='form-control' id='salary'/>
                         </div>
                     </div>
 
                 </div>
                 <br/>
-
-                <div className="row">
-                    <div className="col-12 ">
-                        <button className='btn btn-primary col-12'>Save Customer</button>
-                    </div>
-
-                </div>
-
                 <hr/>
                 <div className="row">
-                    <div className="col-12">
-                        <form className='col-12'>
-                            <input type="search" className='form-control' placeholder='Search Customers here'/>
-                        </form>
+                    <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
+                        <div className="form-group">
+                            <label htmlFor="product">Select Product</label>
+                            <select  id="product" className='form-control'>
+                                <option value="#" disabled defaultValue='Use Option '>Use Option </option>
+                                <option value="#">Customer 01</option>
+                                <option value="#">Customer 02</option>
+                            </select>
+                        </div>
                     </div>
+                    <div className="col-12 col-sm-6 col-md-3" style={styleObj}>
+                        <div className="form-group">
+                            <label htmlFor="description">Product Description</label>
+                            <input type="text" disabled className='form-control' id='description'/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-2" style={styleObj}>
+                        <div className="form-group">
+                            <label htmlFor="price">Unit Price</label>
+                            <input type="number" disabled className='form-control' id='price'/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-2" style={styleObj}>
+                        <div className="form-group">
+                            <label htmlFor="qtyonHand">QTY on Hand</label>
+                            <input type="number" disabled className='form-control' id='qtyonHand'/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-2" style={styleObj}>
+                        <div className="form-group">
+                            <label htmlFor="qty">QTY </label>
+                            <input type="number" className='form-control' id='qty'/>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-12 ">
+                        <button className='btn btn-primary col-12'>Add Product</button>
+                    </div>
+
                 </div>
                 <br/>
                 <div className="row">
@@ -69,31 +101,39 @@ function  Order(){
                             <tr>
                                 <th>#Id</th>
                                 <th>Product Name</th>
-                                <th>QTY On Hand</th>
                                 <th>Unit Price</th>
+                                <th>QTY</th>
+                                <th>Total</th>
+                                <th>Total</th>
                                 <th>Delete Option</th>
                                 <th>Update Option</th>
-                                <th>See more</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr>
                                 <td>#1010</td>
-                                <td>Wimal Bandara</td>
-                                <td>Colombo city</td>
-                                <td>25000.00</td>
+                                <td>Product 01</td>
+                                <td>240</td>
+                                <td>20</td>
+                                <td>2500.00</td>
                                 <td>
-                                    <button className='btn btn-outline-danger btn-sm'>Delete</button>
-                                </td>
-                                <td>
-                                    <button className='btn btn-outline-success btn-sm'>Update</button>
-                                </td>
-                                <td>
-                                    <button className='btn btn-outline-info btn-sm'>View</button>
+                                    <button className='btn btn-outline-danger btn-sm'>Remove</button>
                                 </td>
                             </tr>
                             </tbody>
                         </table>
+                        <br/>
+
+                        <div className="bottom-context" style={bottomContext}>
+                            <div className="total-outer">
+                                <h1>
+                                    Total : 2500.00
+                                </h1>
+                            </div>
+                            <div className="place-order-button-context">
+                                <button className='btn btn-primary'>Place Order </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
