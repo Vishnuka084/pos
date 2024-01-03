463ea15ce91a694e1cff3c802a4756154ec3dfe5
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-03 13:30:50 +0530

    updated Contentn:

--- POS/src/App.tsx
+++ POS/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import './App.css'
 import Home from "./components/Home.tsx";
 import Customer from "./components/Customer.tsx";
@@ -10,22 +10,34 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/customer'>Customer</Link>
-            </li>
-            <li>
-              <Link to='/product'>Product</Link>
-            </li>
-
-          </ul>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <div className="navbar-brand" >
+              <img src="https://th.bing.com/th/id/OIP.KQnzCHlObFW44Qsj4kBN9AHaGT?w=179&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                   alt="" className='logo'/>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
 
-        <hr/>
 
         <Routes>
           <Route path='/' element={<Home/>}/>
